35f0162e9b0ede3c86e2eb5713f6516edb410591
Frederick Obeng-Nyarko <40135934+FreeHill@users.noreply.github.com>
2020-05-30 12:54:03 +0100

    new update

---
+++ public/js/scripts.js
@@ -0,0 +1,14 @@
+(function ($) {
+	/*================================
+    Preloader
+    ==================================*/
+
+	var preloader = $('#preloader');
+	$(window).on('load', function () {
+		setTimeout(function () {
+			preloader.fadeOut('slow', function () {
+				$(this).remove();
+			});
+		}, 300);
+	});
+})(jQuery);
